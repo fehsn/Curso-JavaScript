13e84b07e91a09fe87fac764e1a1641100598b22
Felipe de Souza Nunes <145158740+fehsn@users.noreply.github.com>
2023-11-10 20:51:36 -0300

    atualizando exercicios curso js

---
+++ aulas cursojs/21_estru_condicio_else/script.js
@@ -0,0 +1,8 @@
+let idade= 18;
+let nome= 'Felipe';
+
+if (idade =>18){
+    console.log('Você é maior de idade, pode passar');
+} else {
+    console.log('Você é menor de idade VOLTA PRA CASA');
+}
