2f0cf8b32f1c1b11bc65013e7f9e2049929fcaec
Felipe de Souza Nunes <145158740+fehsn@users.noreply.github.com>
2023-12-11 15:09:42 -0300

    atualizando curso

---
+++ aulas_cursosjs_8/08_insert_before/js/scripts.js
@@ -0,0 +1,6 @@
+let novoElemento = document.createElement("p");
+let elementoAlvo = document.querySelector("#titulo principal");
+let elementoPai = document.querySelector("#container-principal");
+
+elementoPai.insertBefore(novoElemento, elementoAlvo);
+
